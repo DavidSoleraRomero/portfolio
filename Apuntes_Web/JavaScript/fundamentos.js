c622215d9869d5e9f6e379db7537d406586dc0b6
DavidSoleraRomero <soleraromerodavid@gmail.com>
2024-01-28 20:18:29 +0100

    Avances JS

--- Apuntes_Web/JavaScript/fundamentos.js
+++ Apuntes_Web/JavaScript/fundamentos.js
@@ -40,11 +40,36 @@ let variableHoisting2; // A diferencia de con "var", con "let" el intérprete su
 
 /*  Las variables pueden cambiar de tipo; 
     se determina en tiempo de ejecución
+
+    Podemos saber el tipo haciendo uso de "typeof"
     
     Se pueden hacer castings de esta forma:
-    Number(variable)
-    Boolean(variable)
-    String(variable)
-    variable = null
+    Number(variable) --> Transforma a número o a NaN
+    Boolean(variable) --> Si hay contenido true / Sino, false (hay cadena o hay un nº diferente a 0)
+    String(variable) --> Transforma el contenido a cadena
+    variable = null --> Deja de haber valor
     variable = undefined
 */
+
+let address = "Carril de las Castañetas, 36";
+let price = 2.33; 
+let active = false;
+let sample;
+let sampleNull = null;
+
+/*
+console.log(typeof address);
+console.log(typeof price);
+console.log(typeof active);
+console.log(typeof sample);
+console.log(typeof sampleNull); */
+
+let booleanAddress = Boolean(address);
+let numberAddress = Number(address);
+
+console.log(booleanAddress);
+console.log(numberAddress);
+
+
+
+
